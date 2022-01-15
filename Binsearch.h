d6eff3e736b0a858086ad7b8ff5851c73230beea
0exia099 <0exia099@gmail.com>
2022-01-16 06:17:29 +0900

    'update'

--- Binsearch.h
+++
@@ -1,9 +0,0 @@
-//#pragma once
-#ifndef _BINSEARCH_
-#define _BINSEARCH_
-#include <stdio.h>
-
-bool binsearch(int list[], int searchnum, int left, int right, int& count);//이진검색 후 찾으면 1반환, 못찾으면 0반환하는 함수
-int search(int& n, int list[], int listnum[], int& count, int& step);//binsearch함수 실행 후 찾는 수가 없으면 찾는수를 강제로 넣게하는 신호리턴
-//찾는 수가 모두 있으면 1부터n까지 수를 넣은 배열에서 이진검색실행하고 0리턴 하는 함수
-#endif
--- Sort.h
+++
@@ -1,9 +0,0 @@
-//#pragma once
-#ifndef _SORT_
-#define _SORT_
-#include <stdio.h>
-#define SWAP(x,y,t) ((t)=(x), (x)=(y), (y)=(t))//스왑 매크로
-
-void sort(int[], int);//정렬함수
-void Rsort(int[], int);//내림차순 정렬함수
-#endif
